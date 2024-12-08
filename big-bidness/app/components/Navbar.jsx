15e99513cdf9e2d7fd05e9175a48925086c67476
IshmamF <ishmamf2003@gmail.com>
2024-12-07 20:50:37 -0500

    navbar updates based on login status/role

--- big-bidness/app/components/Navbar.jsx
+++ big-bidness/app/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 export const Navbar = () => {
+  const token = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
   const [isToggle, setIsToggle] = useState(false);
 
   function toggleNav() {
@@ -17,21 +19,31 @@ export const Navbar = () => {
         </Link>
         <div className="hidden md:inline-block">
           <div className="space-x-4">
-            <Link href="/create-listing" className="text-black bg-gray-100 hover:bg-black hover:text-white rounded-md px-1 py-1">
-              Create Listing
-            </Link>
-            <Link href="/activity" className="text-black hover:bg-black hover:text-white rounded-md px-1 py-1">
-              Activity
-            </Link>
-            <Link href="/settings" className="text-black hover:bg-black hover:text-white rounded-md px-1 py-1">
-              Settings
-            </Link>
-            <Link href="/auth/login" className="text-black bg-neutral-300 border border-slate-500 hover:bg-zinc-800 hover:text-white rounded-md px-3 py-1">
-              Sign In
-            </Link>
-            <Link href="/auth/register" className="text-white bg-zinc-800 border border-slate-500 hover:bg-neutral-300 hover:text-black rounded-md px-2 py-1">
-              Register
-            </Link>
+            {token ? (
+              <>
+                <Link href="/create-listing" className="text-black bg-gray-100 hover:bg-black hover:text-white rounded-md px-1 py-1">
+                  Create Listing
+                </Link>
+                <Link href="/activity" className="text-black hover:bg-black hover:text-white rounded-md px-1 py-1">
+                  Activity
+                </Link>
+                {(role === 'Admin') && (<Link href="/admin-portal" className="text-black hover:bg-black hover:text-white rounded-md px-1 py-1">
+                  Admin Portal
+                </Link>)}
+                <Link href="/settings" className="text-black hover:bg-black hover:text-white rounded-md px-1 py-1">
+                  Settings
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/auth/login" className="text-black bg-neutral-300 border border-slate-500 hover:bg-zinc-800 hover:text-white rounded-md px-3 py-1">
+                  Sign In
+                </Link>
+                <Link href="/auth/register" className="text-white bg-zinc-800 border border-slate-500 hover:bg-neutral-300 hover:text-black rounded-md px-2 py-1">
+                  Register
+                </Link>
+              </>
+            )}
           </div>
         </div>
         <div className="md:hidden flex items-center">
@@ -46,18 +58,31 @@ export const Navbar = () => {
       {isToggle && (
         <div className="md:hidden">
           <div className="space-y-1 px-2 pb-3">
-            <Link href="/" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
-              Home
-            </Link>
-            <Link href="/about-me" className="text-black block hover:bg-black hover:text-white rounded-lg p-2">
-              About Me
-            </Link>
-            <Link href="/experience" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
-              Experience
-            </Link>
-            <Link href="/contact" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
-              Contact
-            </Link>
+            {token ? (
+              <>
+                <Link href="/create-listing" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Create Listing
+                </Link>
+                <Link href="/activity" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Activity
+                </Link>
+                {(role === 'Admin') && (<Link href="/admin-portal" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Admin Portal
+                </Link>)}
+                <Link href="/settings" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Settings
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/auth/login" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Sign In
+                </Link>
+                <Link href="/auth/register" className="text-black block bg-zinc-100 hover:bg-black hover:text-white rounded-lg p-2">
+                  Register
+                </Link>
+              </>
+            )}
           </div>
         </div>
       )}
